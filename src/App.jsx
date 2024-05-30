d13e81dfefb331bf5a8810af6d88aa90085cbbfb
David Vu <david.vu@berkeley.edu>
2024-05-29 23:40:33 -0700

    Added title text in landing by Ricardo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import * as THREE from "three";
 import SceneInit from "./lib/SceneInit";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import ParticlesCanvas from "./canvas/ParticlesCanvas.jsx"
+
 import "./App.css";
 
 function App() {
     useEffect(() => {
-        const test = new SceneInit("myThreeJsCanvas");
-        test.initialize();
-        test.animate();
+        // const test = new SceneInit("myThreeJsCanvas");
+        // test.initialize();
+        // test.animate();
 
         // const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
         // const boxMaterial = new THREE.MeshNormalMaterial();
@@ -17,35 +19,31 @@ function App() {
         // test.scene.add(boxMesh);
 
         // spotlight
-        const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
-        spotLight.position.set(0, 25, 0);
-        spotLight.castShadow = true;
-        spotLight.shadow.bias = -0.0001;
-        test.scene.add(spotLight);
+        // const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
+        // spotLight.position.set(0, 25, 0);
+        // spotLight.castShadow = true;
+        // spotLight.shadow.bias = -0.0001;
+        // test.scene.add(spotLight);
 
         // elaina
-        const loader = new GLTFLoader();
-        loader.load('/elaina/scene.gltf', (gltf) => {
-                const mesh = gltf.scene;
-                test.scene.add(mesh);
-             },
-            (xhr) => {
-                console.log(`loading ${(xhr.loaded / xhr.total) * 100}%`);
-            },
-            (error) => {
-                console.error(error);
-            }
-        );
+        // const loader = new GLTFLoader();
+        // loader.load('/elaina/scene.gltf', (gltf) => {
+        //         const mesh = gltf.scene;
+        //         test.scene.add(mesh);
+        //      },
+        //     (xhr) => {
+        //         console.log(`loading ${(xhr.loaded / xhr.total) * 100}%`);
+        //     },
+        //     (error) => {
+        //         console.error(error);
+        //     }
+        // );
+
     }, []);
 
     return (
         <div>
-            <canvas id="myThreeJsCanvas" />
-            <div className="magic">
-              <div className="playground">
-                
-              </div>
-            </div>
+            <ParticlesCanvas/>
         </div>
     );
 }
